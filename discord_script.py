899cb1d09e9f3486ef5b10dfcb19a450ead26a74
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-05-22 21:11:03 +0530

    working for first time

--- discord_script.py
+++ discord_script.py
@@ -2,7 +2,7 @@ import gmailapicall
 import discord
 import os
 import mail_to_pdf
-from dotenv import load_dotenv
+from dotenv import load_dotenv  
 load_dotenv()
 client=discord.Client()
 text=gmailapicall.mainn()
@@ -11,16 +11,9 @@ if(len(text)==2):
     mail_to_pdf.topdf(text[0])
 else:
     mail_to_pdf.topdf(text)
-#mail_to_pdf.topdf(text[0])
 @client.event
-async def on_ready():
-    print("we have logged in as {0.user}".format(client))
-
-@client.event
-async def on_message(message):
-    if message.author==client.user:
-        return 
-    if message.content.startswith('!hello'): 
-        await message.channel.send(file=discord.File('gmail.pdf'))
-        # await bot.send_file(gmail,'gmail.pdf')
+async def on_ready(): 
+    channel = client.get_channel(842160648058306593) #  Gets channel from internal cache
+    await channel.send("hello world")
+    await channel.send(file=discord.File("gmail.pdf"))
 client.run(os.getenv('token'))
